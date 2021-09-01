b00db6f5a8ddd09f4262a3b6d20b7233d049db48
Nikita <barhatenikita@gmail.com>
2021-09-01 13:03:38 +0530

    Initial Commit

---
+++ CollapsibleTableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollapsibleTableview
+//
+//  Created by Nikita Vilas Barhate on 01/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
